61dae4e40352a6c807223f6a12d5ad4de5299263
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-05 12:07:23 -0700

    adding kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,20 @@ from datetime import datetime
 
 class BaseModel:
     """ Our class for Base """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ initalizes new instance of basemodel """
-        self.id = str(uuid.uuid4())
-        self.created_at = (datetime.now())
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                my_list = ["created_at, updated_at"]
+                if key in my_list:
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = (datetime.now())
+            self.updated_at = self.created_at
 
     def save(self):
         """ updates 'updated_at with current datetime """
@@ -31,3 +40,4 @@ class BaseModel:
         """ string rep of instance """
         class_name = self.__class__.__name__
         return f"[{class_name}] ({self.id}) {self.__dict__}"
+
