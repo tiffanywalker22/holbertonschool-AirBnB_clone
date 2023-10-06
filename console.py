2abcacbcec195fa7cef13ad0d10988c774e04981
tiffanywalker22 <tbcasper16@gmail.com>
2023-10-06 09:07:53 -0700

    adjusting documentation

--- console.py
+++ console.py
@@ -7,15 +7,15 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """ prompt for cmd interpreter """
+    """prompt for cmd interpreter"""
     prompt = "(hbnb)"
 
     def do_quit(self, arg):
-        """ Quit command to exit the program """
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """ exits program using ctrl+d (EOF) """
+        """exits program using ctrl+d (EOF)"""
         print("")
         return True
 
