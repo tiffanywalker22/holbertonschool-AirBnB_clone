73233eac1b00905a8d3c4b8f6d24ca0fec8ebcf3
Zal-atan <ethanzalta@gmail.com>
2023-10-06 11:13:35 -0700

    Fixing error in file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,11 @@ import os
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class FileStorage:
@@ -43,6 +48,6 @@ class FileStorage:
                 for key, value in objDict.items():
                     self.__objects[key] = eval(value["__class__"])(**value)
         except Exception as e:
-            print(e)
+            pass
 
     allowed_classes = [BaseModel, User]
